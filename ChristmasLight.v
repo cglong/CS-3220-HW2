e7f0c2031ed5e78f725030bd43d4a21b804208d7
Chris Long <chrislong@gatech.edu>
2013-02-01 00:28:21 -0500

    Press KEY2 to reset speed

--- ChristmasLight.v
+++ ChristmasLight.v
@@ -56,6 +56,9 @@ module ChristmasLight(LEDG, LEDR, CLOCK_50, KEY);
 		if (!KEY[1])
 			if (TotalCnt >= 32'd12500000)
 				TotalCnt = TotalCnt - 32'd12500000;
+		
+		if (!KEY[2])
+			TotalCnt = 32'd25000000;
 	end
 	
 	assign LEDR = redValues;
