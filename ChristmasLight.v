c0e473ab84804f33acd07c531026397cce8772f5
Lee Benson Martin <lmartin41@gatech.edu>
2013-02-01 12:14:37 -0500

    mostly working
    key[0] slows, key[1] speeds up and key[2] resets like they should, but
    there are random bugs that happen.  sometimes pressing things too fast
    makes no lights appear at all and sometimes they stay on for too long.
    can u look over this code and find any errors?  let's talk to the
    professor during office hours today

--- ChristmasLight.v
+++ ChristmasLight.v
@@ -11,14 +11,16 @@ module ChristmasLight(LEDG, LEDR, CLOCK_50, KEY);
 	reg [9:0] ledValues;
 	reg [15:0] ledCounter;
 	reg [15:0] state;
+	reg [0:0] block;
 	
 	initial Cnt = 0;
 	initial TotalCnt = 32'd25000000;
 	initial greenValues = 0;
 	initial redValues = 0; 
 	initial ledValues = 0;
-	initial ledCounter = 1;
+	initial ledCounter = 0;
 	initial state = 0;
+	initial block = 0;
 	
 	always @(posedge CLOCK_50) begin
 		Cnt <= Cnt + 32'd1;
@@ -31,6 +33,7 @@ module ChristmasLight(LEDG, LEDR, CLOCK_50, KEY);
 		if (ledCounter == 6) begin
 			state <= (state + 1)  % 3;
 			ledCounter <= 0;
+			block <= 0;
 		end
 		
 		if (state == 0) begin
@@ -48,20 +51,24 @@ module ChristmasLight(LEDG, LEDR, CLOCK_50, KEY);
 			redValues <= ledValues;
 		end
 		
-		if (!KEY[0]) begin
-			if (TotalCnt <= 32'd250000000)
-				TotalCnt <= TotalCnt + 32'd12500000;
-		end
+		if (block == 0) begin
+			if (!KEY[0]) begin
+					block <= 1;
+					if (TotalCnt <= 32'd250000000)
+						TotalCnt <= TotalCnt + 32'd12500000;
+			end
 	
-		if (!KEY[1]) begin
-			if (TotalCnt >= 32'd12500000)
-				TotalCnt <= TotalCnt - 32'd12500000;
-		end
+			if (!KEY[1]) begin
+				block <= 1;
+				if (TotalCnt >= 32'd12500000)
+					TotalCnt <= TotalCnt - 32'd12500000;
+			end
  		
-		if (!KEY[2]) begin
-			TotalCnt <= 32'd25000000;
+			if (!KEY[2]) begin
+				block <= 1;
+				TotalCnt <= 32'd25000000;
+			end
 		end
-
 	end
 	
 	assign LEDR = redValues;
